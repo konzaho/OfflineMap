00ccf9f229e3df1afdbffbce32bd021950b7a55c
konzaho <sinister.horde@gmail.com>
2015-06-25 13:50:36 +0700

    second commit by konzaho. added offline map

--- app/src/main/java/com/palapa/company/offlinemap/MainActivity.java
+++ app/src/main/java/com/palapa/company/offlinemap/MainActivity.java
@@ -5,13 +5,106 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends ActionBarActivity {
+import com.palapa.company.offlinemap.map.MFMapView;
+import com.palapa.company.offlinemap.map.MFTileModuleProvider;
+import com.palapa.company.offlinemap.map.MFTileSource;
+
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.ResourceProxy;
+import org.osmdroid.tileprovider.IRegisterReceiver;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.util.GeoPoint;
+
+import java.io.File;
+import java.math.BigInteger;
+import com.palapa.company.offlinemap.R;
+
+
+public class MainActivity extends ActionBarActivity implements IRegisterReceiver {
+    MFMapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidGraphicFactory.createInstance(this.getApplication());
+
+
+        try {
+            ResourceProxy resourceProxy = new DefaultResourceProxyImpl(this);
+
+            // Create a custom tile source
+            final ITileSource tileSource = new MFTileSource(8,20, 256,
+                    Environment.getExternalStorageDirectory() + "/Mapsforge/map/indonesia.map",
+                    Environment.getExternalStorageDirectory() + "/Mapsforge/renderthemes/rendertheme-v4.xml"
+                    , this);
+//
+            final IRegisterReceiver registerReceiver = new SimpleRegisterReceiver(this);
+
+
+
+            MFTileModuleProvider moduleProvider;
+            moduleProvider = new MFTileModuleProvider(this, new File(""), (MFTileSource)tileSource);
+
+
+
+            MapTileModuleProviderBase[] pTileProviderArray;
+            pTileProviderArray = new MapTileModuleProviderBase[] { moduleProvider};
+
+
+            final MapTileProviderArray tileProviderArray = new MapTileProviderArray(
+                    tileSource, registerReceiver, pTileProviderArray);
+
+            mapView = new MFMapView(this, tileProviderArray.getTileSource().getTileSizePixels(),
+                    resourceProxy, tileProviderArray);
+
+
+            mapView.setBuiltInZoomControls(false);
+            mapView.setMultiTouchControls(true);
+
+
+            final LinearLayout mapLinearLayout = (LinearLayout) this.findViewById(R.id.mapLinearLayout);
+
+            mapLinearLayout.addView(mapView);
+
+
+            mapView.setCurrentAsCenter();
+            mapView.addMyLocationOverlay();
+
+            mapView.setBuiltInZoomControls(true);
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
     }
 
     @Override
